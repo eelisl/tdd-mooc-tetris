a337f2ee0da4118a9dc4d235343fcd5f948f3459
eelis <dev@sovian.fi>
2026-01-29 21:25:14 +0200

    feat: init the shape

--- src/RotatingShape.mjs
+++ src/RotatingShape.mjs
@@ -1,4 +1,9 @@
 export class RotatingShape {
     shape = [];
 
+    constructor(shape) {
+        const rows = shape.replace(/\s+/g, "\n").split("\n");
+        this.shape = rows.map(row => row.split(""));
+    }
+
 }
