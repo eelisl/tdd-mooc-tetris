51108d0b665c5518a43c5a2055fa48477c7841f8
eelis <dev@sovian.fi>
2026-01-29 21:05:37 +0200

    feat: fallingBlock variable

--- src/Board.mjs
+++ src/Board.mjs
@@ -2,11 +2,20 @@ export class Board {
   width;
   height;
   board = [];
+  fallingBlock = null;
 
   constructor(width, height) {
     this.width = width;
     this.height = height;
     this.board = Array(height).fill(this.createRow());
+  }
+
+  createFallingBlock(block) {
+    this.fallingBlock = {
+      x: Math.floor(this.width / 2),
+      y: 0,
+      block,
+    };
   }
 
   createRow() {
