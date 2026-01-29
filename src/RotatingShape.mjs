98ceac1fd62ebb635535a4321ea67193c9d7d327
eelis <dev@sovian.fi>
2026-01-29 21:42:31 +0200

    feat: rotate block right naively

--- src/RotatingShape.mjs
+++ src/RotatingShape.mjs
@@ -10,6 +10,16 @@ export class RotatingShape {
         return new RotatingShape(shape);
     }
 
+    rotateRight() {
+        const rotatedShape = this.shape.map((row, index) => {
+            return row.map((_, i) => {
+                return this.shape[this.shape.length - 1 - i][index];
+            });
+        })
+        this.shape = rotatedShape;
+        return this;
+    }
+
     toString() {
         return this.shape.map(row => row.join("")).join("\n") + "\n";
     }
